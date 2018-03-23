b778e31cb4652962f4db28c591d330231909da13
Thomas Kaegi <tk@walderwerber.ch>
2018-03-23 07:37:35 +0100

    Fixes #2

--- uninstall.sql
+++ uninstall.sql
@@ -1,1 +1,4 @@
 DROP TABLE IF EXISTS `%TABLE_PREFIX%media_manager_plus_breakpoints`;
+
+ALTER TABLE `%TABLE_PREFIX%media_manager_type` DROP COLUMN `group`;
+ALTER TABLE `%TABLE_PREFIX%media_manager_type` DROP COLUMN `subgroup`;
