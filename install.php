7f086f1c697237854eca79c68e065ec58973fb64
Markus Schnieder <33157452+mschnieder@users.noreply.github.com>
2022-08-20 15:15:04 +0200

    Wip v2.3.0 (#41)
    * Version 2.2.0
    - Update der LazyLoad Library von Version 4.0.0 auf Version 5.3.2
    - Neue Funktion mmp::getLazyLoad() zum einbinden der jeweiligen Version der LazyLoad Library (siehe Doku)
    - Anpassung der filenamesByBreakpoint
    - Anpassung der Doku
    
    * Funktionserweiterung
    - Sortierung von Breakpoints
    - Bereinigung von media_manager_type beim löschen von Breakpoints
    - Bereinigung von media_manager_type_effect beim löschen von Breakpoints
    - Anpassung der Doku
    
    * Versions Anpassung
    
    Co-authored-by: markus@dermarkus.com <!R1e5i7U8SFMKx6ssw3x2Tk>

--- install.php
+++ install.php
@@ -1,5 +1,28 @@
 <?php
+$addon = rex_addon::get('media_manager_plus');
+
+
 	rex_sql_table::get(rex::getTable('media_manager_type'))
 	->ensureColumn(new rex_sql_column('group', 'int(10)'))
 	->ensureColumn(new rex_sql_column('subgroup', 'int(10)'))
+    ->ensureColumn(new rex_sql_column('breakpointId', 'int(10)'))
 	->alter();
+
+
+    rex_sql_table::get(rex::getTable('media_manager_plus_breakpoints'))
+        ->ensureColumn(new rex_sql_column('prio', 'int(10)'))
+        ->ensureColumn(new rex_sql_column('createdate', 'datetime'), 'prio')
+        ->ensureColumn(new rex_sql_column('createuser', 'varchar(255)'), 'createdate')
+        ->ensureColumn(new rex_sql_column('updatedate', 'datetime'), 'createuser')
+        ->ensureColumn(new rex_sql_column('updateuser', 'varchar(255)'), 'updatedate')
+        ->alter();
+
+    $s = rex_sql::factory();
+    $breakpoints = $s->getArray('Select * from '.rex::getTablePrefix().'media_manager_plus_breakpoints order by id');
+    $i = 1;
+    foreach($breakpoints as $bp) {
+        if($bp['prio'] === 0) {
+            $s->setQuery("Update " . rex::getTablePrefix() . "media_manager_plus_breakpoints set prio=" . $i . " where id=".$bp['id']);
+            $i++;
+        }
+    }
